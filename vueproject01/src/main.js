53898a8d133445beb1ff8d37f4baf374b54d0430
haran seok <gkfks444@naver.com>
2023-02-07 19:16:44 +0900

    i18n 설정

--- vueproject01/src/main.js
+++ vueproject01/src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import Router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import i18n from './i18n'
 
 const app = createApp(App)
 
 loadFonts()
-  app.use(Store)
-  app.use(Router)
-  app.use(vuetify)
-  app.mount('#app')
+app.use(Store)
+app.use(Router)
+app.use(vuetify)
+app.use(i18n)
+app.mount('#app')
--- vueproject01/src/pages/RND/LocaleRnd.vue
+++ vueproject01/src/pages/RND/LocaleRnd.vue
@@ -1,16 +1,41 @@
 <template>
     <article>
-        locale
+        <h2>locale</h2>
+        <section>
+            <div class="text_box">
+                <h3>{{ $t('title') }}</h3>
+                <p>{{ $t('message') }}</p>
+            </div>
+            <button @click="$i18n.locale='ko'">한국어</button>
+            <button @click="$i18n.locale='en'">english</button>
+        </section>
     </article>
 </template>
 <script>
     export default {
-        
+        data(){
+            return {
+                
+            }
+        }
     }
 </script>
-
 <style lang="scss" scoped>
     article{
+        width: 80%;
+        margin: 0 auto;
         padding: 3%
     }
+    section{
+        margin: 20px 0;
+    }
+    button{
+         padding: 5px 10px;
+         margin: 0 5px;
+         border-radius: 3px;
+         background: rgb(0, 77, 95);
+         color: #eee;
+         text-transform: capitalize;   
+        }
+
 </style>
