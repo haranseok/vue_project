3dd24cfc3c4910fe0b33e4053d6050fcf43231cf
haran seok <gkfks444@naver.com>
2023-02-08 15:56:08 +0900

    board 컴포넌트 생성 및 검색 input 공통 컴포넌트화

---
+++ vueproject01/src/components/item/searchInput.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="search_box jcsb">
+        <input type="text" 
+            v-model="search"
+            placeholder="검색어를 입력해주세요.">
+        <v-icon @click="searchEnter">mdi-magnify</v-icon>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            search: '',
+        }
+    },
+    methods: {
+        searchEnter(){
+            this.$emit('searchEvent', this.search)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.search_box{
+    width: 230px;
+    padding:5px 8px;
+    border-radius: 3px;
+    box-shadow: 1px 1px 1px 1px rgba(0,0,0,0.2);
+    input{
+        width: 200px;
+        overflow:hidden ;
+        border-radius: 3px;
+        &::placeholder{
+            font-size: 14px;
+            color: #cecccc;
+        }
+    }
+    .v-icon{
+        color: #aaaaaa;
+        padding: 5px;
+        cursor: pointer;
+    }
+}
+
+</style>
