7750d44edc3ea9a96c77eab4111220a58b0d259d
haran seok <gkfks222@gmail.com>
2023-02-27 16:40:02 +0900

    페이스북 로그인 기능 구현

--- vueproject01/src/pages/RND/SocialLoginRnd.vue
+++ vueproject01/src/pages/RND/SocialLoginRnd.vue
@@ -2,12 +2,24 @@
     <div class="container">
         <div id="naver_id_login"></div>
         <v-btn id="custom-login-btn" color="#FEE500" @click="doKakaoLogin()" prepend-icon="mdi-chat">카카오톡 로그인</v-btn>
-            <div @click="googleLogin">google login</div>
+        <v-btn @click="googleLogin">google login</v-btn>
+        <v-btn @click="facebookLogin">facebook login</v-btn>
     </div>
 </template>
 <script>
 import { googleTokenLogin } from "vue3-google-login"
 export default {
+    created(){
+        window.fbAsyncInit = function() {
+            window.FB.init({
+            appId : '968088867934488',
+            cookie : true,
+            xfbml : true,
+            version : 'v9.0'
+            });
+            window.FB.AppEvents.logPageView();         
+        };
+    },
   mounted(){
     // naver
     
@@ -28,20 +40,37 @@ export default {
         this.$router.push('/naver')
     }
   },
-  methods: {
-    doKakaoLogin() {
+    methods: {
+        doKakaoLogin() {
             const params = {
                 redirectUri: "http://192.168.0.90:81/user",
             };
             window.Kakao.Auth.authorize(params);
         },
-    // google http://localhost:81/ ip주소로 등록이 안되어서 로컬호스트 url등록했음.
-    googleLogin(){
+        // google http://localhost:81/ ip주소로 등록이 안되서 로컬호스트 url등록
+        googleLogin(){
             googleTokenLogin().then((response)=>{
                 console.log(response)
             })
+        },
+        facebookLogin() {
+            window.FB.login(function(response) {
+                console.log('access_token:'+ response.authResponse.accessToken)
+        if (response.status === 'connected') {
+          window.FB.api('/me', 'get', {fields: 'name,email'}, function(r) {
+
+            const facebook_email = r.email;
+            const facebook_name = r.name;
+
+            console.log(facebook_email);
+            console.log(facebook_name);
+
+          })
         }
-  }
+      }, {scope: 'public_profile,email'})
+        }
+        
+    }
 
 }
 </script>
